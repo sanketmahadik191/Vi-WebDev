8dd34576df0dd671c00744eb240c6c875a61bdc0
sundar <sundarchaudhary038gmail.com>
2024-09-13 18:58:44 +0530

    completed navbar & home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="flex justify-between items-center bg-[#134074] text-white h-[13vh]" >
+      <div className="ml-20">
+        <h1 className='text-3xl'>V O R K I N S T A</h1>
+        <h6 className='text-sm text-gray-300'>WE BUILD ENTREPRENEURS</h6>
+      </div>
+      <div className="flex mr-20 text-lg font-normal">
+        <ul className="flex ">
+          <li><a className="ml-10 text-2xl font-bold border-b-4">HOME</a></li>
+          <li><a className="ml-10 ">About Us</a></li>
+          <li><a className="ml-10 ">FAQ</a></li>
+        </ul>
+        <button className="ml-10  bg-white font-medium text-blue-950 px-4 rounded-lg" >Login</button>
+        <button className="ml-10 bg-white font-medium text-blue-950 px-4 rounded-lg">Sign Up</button>
+        
+        
+      </div>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
