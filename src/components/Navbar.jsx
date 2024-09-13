c3ab984e1bd6104a8652ebaca8eaa45ded60ec7b
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2024-09-13 15:00:21 +0530

    routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>Navbar
+ <Link to='/login'>
+ <button className='p-4 bg-slate-300'>Login</button>
+ </Link>
+
+ <Link to='/signup'>
+ <button className='p-4 bg-slate-400'>Signup</button>
+ </Link>
+       
+    </div>
   )
 }
 
