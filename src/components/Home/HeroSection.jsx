d4aff8dde0a47c9f747365c600968f66574f5341
sundar <sundarchaudhary038gmail.com>
2024-09-14 17:35:20 +0530

    Updated components for responsiveness

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -2,31 +2,34 @@ import React from 'react'
 import img from '../../assets/Home-img.jpeg'
 
 
+
 function HeroSection() {
   return (
-    <section className="flex items-center justify-evenly py-20 text-center">
-      <div className='w-[40%]'>
-      <p className="text-4xl mb-8 ">
+    <section className=" md:flex items-center justify-evenly py-20 text-center">
+      <div className='md:w-[40%] text-xl lg:text-2xl xl:text-3xl'>
+      <p className=" mb-8 ">
         Now, You Are In The Hands Of </p>
-        <h1 className="text-[#134074] text-[4rem]  mb-4 font-extrabold">My VORKINSTA</h1>
+        <h1 className="text-[#0D3451] text-[2rem] lg:text-[3rem] mb-8 font-extrabold">My VORKINSTA</h1>
       
-      <p className="text-4xl mb-10  font-bold">
+      <p className=" mb-8  font-bold">
         Crafts The Future Of Work With Enduring Excellence
       </p>
-      <div className="flex  mb-10">
-          <input type="text" className="border-2 py-4 px-52 rounded-l-full rounded-r-none w-64 bg-gray-100"/>
-          <button className="bg-black text-xl text-white px-12 py-4 rounded-r-full rounded-l-none">Search</button>
+      <div className="flex justify-center">
+      <div className="flex text-xl mb-10">
+          <input type="text"  className="border-2 bg-gray-50 py-3 px-4  rounded-l-full rounded-r-none w-50 lg:w-80 outline-none " placeholder='Search'></input>
+          <button className="bg-black text-white px-4 lg:px-12 py-3 rounded-r-full rounded-l-none">Search</button>
         </div>
-      <div className="flex justify-center space-x-4 mb-10 text-3xl font-semibold">
-        <button className="px-12 py-5 bg-[#134074] border-[#134074] rounded-full text-white  ">
+        </div>
+      <div className="flex justify-center items-center space-x-4 mb-10 text-xl lg:text-[1.3rem] lg:font-semibold">
+        <button className="px-4 py-3 lg:px-12 lg:py-4 border bg-[#0D3451] border-[#0D3451] rounded-full text-white ">
           Join Now
         </button>
-        <button className="px-8 py-5 border border-[#134074] rounded-full text-[#134074]">
+        <button className="px-4 py-3 lg:px-12 lg:py-4 border border-[#0D3451] rounded-full text-[#0D3451]">
           Contact Us
         </button>
       </div>
       </div>
-      <div className="w-[40%]">
+      <div className="w-full md:w-[40%] flex justify-center p-10 md:p-0">
         <img src={img} alt='images' className='rounded-lg'></img>
       </div>
     </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
+import logo from'../assets/Vorkinsta logo.png'
+import { HiMenu } from "react-icons/hi";
 
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center bg-[#134074] text-white h-[13vh]" >
-      <div className="ml-20">
-        <h1 className='text-3xl'>V O R K I N S T A</h1>
-        <h6 className='text-sm text-gray-300'>WE BUILD ENTREPRENEURS</h6>
+    <nav className="flex justify-between items-center bg-[#0D3451] text-white h-[13vh]" >
+      <div className="ml-12 lg:ml-20">
+        <img src={logo} alt="Vorkinsta logo" className='w-[10rem] lg:w-[15rem]'></img>
       </div>
-      <div className="flex mr-20 text-lg font-normal">
-        <ul className="flex ">
-          <li><a className="ml-10 text-2xl font-bold border-b-4">HOME</a></li>
-          <li><a className="ml-10 ">About Us</a></li>
-          <li><a className="ml-10 ">FAQ</a></li>
+      <div>
+      <HiMenu className='md:hidden mr-12 text-3xl'/>
+      </div>
+      <div className="md:flex mr-12 lg:mr-20 text-xl font-normal hidden">
+        <ul className="flex gap-6 lg:gap-12">
+          <li><a >Home</a></li>
+          <li><a >About Us</a></li>
+          <li><a >FAQ</a></li>
         </ul>
-        <button className="ml-10  bg-white font-medium text-blue-950 px-4 rounded-lg" >Login</button>
-        <button className="ml-10 bg-white font-medium text-blue-950 px-4 rounded-lg">Sign Up</button>
-        
-        
+        <button className="ml-5 lg:ml-10  bg-white font-medium text-[#0D3451] px-2 lg:px-4  rounded-lg" >Login</button>
+        <button className="ml-5 lg:ml-10 bg-white font-medium text-[#0D3451] px-2 lg:px-4  rounded-lg">Sign Up</button>
       </div>
     </nav>
   )
