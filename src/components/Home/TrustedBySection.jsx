59a876a3a7aa6e3b5dd0c269d1f535c9441bab8f
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2024-09-14 19:52:06 +0530

    updated navbar and hero section

--- src/components/Home/TrustedBySection.jsx
+++ src/components/Home/TrustedBySection.jsx
@@ -10,7 +10,7 @@ function TrustedBySection() {
       <h2 className="text-4xl font-bold mb-8 text-gray-700">
         Trusted By
       </h2>
-      <div className="flex justify-center items-center flex-wrap space-x-10 sm:space-x-14 lg:space-x-20">
+      <div className="flex justify-center items-center flex-wrap space-x-6 sm:space-x-12 lg:space-x-20">
         <span className="h-16 w-16 flex items-center justify-center text-6xl"><FcGoogle /></span>
         <span className="h-16 w-16 flex items-center justify-center text-6xl"><IoLogoMicrosoft /></span>
         <span className="h-16 w-16 flex items-center justify-center text-6xl"><SiIbm /></span>
