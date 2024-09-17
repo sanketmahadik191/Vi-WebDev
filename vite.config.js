a3ebf88539559d37267755f1726b95b057f914f6
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2024-09-17 15:17:07 +0530

    configuring environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const port = process.env.VITE_APP_PORT ? parseInt(process.env.VITE_APP_PORT, 10) : 3000;
+const backendLink = process.env.VITE_APP_BACKEND_LINK || 'http://localhost:5000';
+
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port,
+    proxy: {
+      '/api': {
+        target: backendLink,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
